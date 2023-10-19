25147727659957b2bb64cf25299fee70ebfd76af
Jerry Tarus <jerrytarus@gmail.com>
2023-10-19 12:19:05 +0300

    Fetch request initiated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const init = () => {
         event.preventDefault();
         console.log(event);
         const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
   
 }
 
